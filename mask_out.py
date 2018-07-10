Add correlation and visualization, fix masking out

mask_out.py
@@ -16,17 +16,19 @@ import onmt.model_builder
 import onmt.modules
 import onmt.opts
 
+import os
+
 
 def main(opt):
-    translator = build_translator(opt, report_score=True, logger=logger)
+    translator = build_translator(opt, report_score=True, logger=logger, use_output=False)
 
     # We cannot run without the following arguments:
     assert opt.mask_out_layer != -1
     assert opt.mask_out_basis != ""
 
     # Load in the basis
-    basis = torch.load(opt.mask_out_basis)
-    inverse_basis = torch.transpose(basis)
+    basis = torch.load(opt.mask_out_basis).cuda()
+    inverse_basis = basis.t()
 
     h, h = basis.shape
 
@@ -34,7 +36,7 @@ def main(opt):
         # Note that layer is here a padded matrix of size
         # (sentences) x (tokens) x (hidden_size).
         # To zero out everything we'll need to reshape twice.
-        if i == opt.mask_out_layer
+        if i == opt.mask_out_layer:
             # Project onto the space spanned by basis * mask.
             s, t, h = layer.shape
             layer = torch.reshape(layer, (s * t, h))
@@ -48,15 +50,18 @@ def main(opt):
 
     def run_with(first, last):
         mask = torch.eye(h)
-        for j in range(first, last)
+        for j in range(first, last):
             mask[j] = 0
+        mask = mask.cuda()
 
-        translator.translate(src_path=opt.src,
-                             tgt_path=os.path.join(opt.tgt, 'without-%d-%d.txt' % (first, last)),
-                             src_dir=opt.src_dir,
-                             batch_size=opt.batch_size,
-                             attn_debug=opt.attn_debug,
-                             intervention=lambda l, i: intervene(l, mask,i))
+        with open(os.path.join(opt.output, 'without-%d-%d.txt' %
+                (first, last)), 'w') as out_file:
+            translator.translate(src_path=opt.src,
+                                 src_dir=opt.src_dir,
+                                 batch_size=opt.batch_size,
+                                 attn_debug=opt.attn_debug,
+                                 intervention=lambda l, i: intervene(l, mask,i),
+                                 out_file=out_file)
 
     if opt.mask_out_cumulative:
         # Remove top

onmt/translate/translator.py
@@ -18,8 +18,8 @@ import onmt.inputters as inputters
 import onmt.opts as opts
 
 
-def build_translator(opt, report_score=True, logger=None, out_file=None):
-    if out_file is None:
+def build_translator(opt, report_score=True, logger=None, out_file=None, use_output=True):
+    if out_file is None and use_output:
         out_file = codecs.open(opt.output, 'w+', 'utf-8')
 
     if opt.gpu > -1:
@@ -147,7 +147,8 @@ class Translator(object):
                   src_dir=None,
                   batch_size=None,
                   attn_debug=False,
-                  intervention=None):
+                  intervention=None,
+                  out_file=None):
         """
         Translate content of `src_data_iter` (if not None) or `src_path`
         and get gold scores if one of `tgt_data_iter` or `tgt_path` is set.
@@ -215,32 +216,35 @@ class Translator(object):
 
         for batch in data_iter:
             if self.dump_layers != '':
-                batch_data, dumped_layers = self.translate_batch(batch, data, intervention=intervention)
+                batch_data, dumped_layers = self.translate_batch(batch,
+                        data, intervention=intervention)
+
+                # Get the correct order of sentences so that we can dump in
+                # the same order as input occurred.
+                inds, perm = torch.sort(batch_data['batch'].indices.data)
 
                 # At this point dumped_layers is going to be an array of
-                # (num_layers) packed sequences, each of which has (batch)
+                # (num_layers) packed sequences, each of which has (len) x (batch)
                 # shape. We would like to transpose this, so that
                 # we have an array of "sentences", each of which is
                 # an array of "tokens", each of which is an array of "layers",
                 # each of which is an array of "neurons".
                 dumped_layers = [unpack(layer) for layer in dumped_layers] # Tuples of (tensor, lengths)
-                print(dumped_layers[0][0].shape)
-                print(dumped_layers[0][1])
                 dumped_layers = [
                     [
                         [
                             # Array of layers
-                            dumped_layers[i][0][t][s]
+                            dumped_layers[i][0][t][idx]
                             for i in range(len(dumped_layers))
                         ]
 
                         # Array of tokens; dumped_layers[0][1] is the list of
                         # sentence lengths for the batch, so we can look up
                         # number of tokens here
-                        for t in range(dumped_layers[0][1][s])
+                        for t in range(dumped_layers[0][1][idx])
                     ]
                     # Array of sentences
-                    for s in range(dumped_layers[0][0].shape[1])
+                    for idx in perm
                 ]
 
                 # Accumulate all the dumped layers into one big list of sentences.
@@ -261,8 +265,13 @@ class Translator(object):
                 n_best_preds = [" ".join(pred)
                                 for pred in trans.pred_sents[:self.n_best]]
                 all_predictions += [n_best_preds]
-                self.out_file.write('\n'.join(n_best_preds) + '\n')
-                self.out_file.flush()
+
+                if out_file is None:
+                    self.out_file.write('\n'.join(n_best_preds) + '\n')
+                    self.out_file.flush()
+                else:
+                    out_file.write('\n'.join(n_best_preds) + '\n')
+                    out_file.flush()
 
                 if self.verbose:
                     sent_number = next(counter)